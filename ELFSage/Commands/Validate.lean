98b85f26eaa1fa25e7d31b299f8ef4555d46ba61
Graham <gleachkr@gmail.com>
2024-04-18 13:29:08 -0400

    Add more functionality to validate subcommand

--- ELFSage/Commands/Validate.lean
+++ ELFSage/Commands/Validate.lean
@@ -3,7 +3,10 @@ import ELFSage.Util.Cli
 
 import ELFSage.Types.ELFHeader
 import ELFSage.Types.File
-
+import ELFSage.Types.Note
+import ELFSage.Types.Dynamic
+import ELFSage.Types.Relocation
+import ELFSage.Types.SymbolTable
 
 def mkRawProgramHeaderErrors
   [ELFHeader α]
@@ -25,19 +28,127 @@ def mkRawSectionHeaderErrors
   let is64Bit := ELFHeader.is64Bit eh
   List.map (λoffset ↦ mkRawSectionHeaderTableEntry? bytes is64Bit isBigendian offset) shoffsets
 
+def getSectionNamesOrErr
+  [ELFHeader α] (eh : α)
+  [SectionHeaderTableEntry β] (sht: List (Except String β))
+  (bytes : ByteArray)
+  : Except String ELFStringTable :=
+    let shstrndx := ELFHeader.e_shstrndx eh
+    if h : shstrndx ≥ sht.length
+    then .error $
+       s!"the shstrndx from the elf header requests section {ELFHeader.e_shstrndx eh}, " ++
+       s!"but the there are only {sht.length} section header table entries indicated."
+    else match  sht[shstrndx] with
+      | .error e => .error
+           $ s!"the shstrndx from the elf header requests section {ELFHeader.e_shstrndx eh}, "
+          ++ s!"but that section was unreadable, with the following error: {e}"
+      | .ok shte => do
+      let shstr_start := SectionHeaderTableEntry.sh_offset shte
+      let shstr_end := shstr_start + SectionHeaderTableEntry.sh_size shte
+      if shstr_end > bytes.size
+      then .error $
+          s!"The section header name string table offset 0x{Hex.toHex shstr_start}, and endpoint 0x{Hex.toHex shstr_end} " ++
+          s!"runs off the end of the binary, which contains only 0x{Hex.toHex bytes.size} bytes."
+      else .ok ⟨bytes.extract shstr_start shstr_end⟩
+
+def printNotesErrs
+  [ELFHeader α] (eh : α)
+  (shte: RawSectionHeaderTableEntry)
+  (sec: InterpretedSection)
+  (idx : Nat)
+  : IO Unit := recur (SectionHeaderTableEntry.sh_size shte) 0 1
+  where
+    alignTo4 n := n + (n % 4)
+    recur space offset count :=
+      match space with
+      | 0 => pure ()
+      | spaceminus + 1 => --we work with space-1 to automatically prove termination
+        match mkRawNoteEntry?
+          (sec.section_body)
+          (ELFHeader.isBigendian eh)
+          (ELFHeader.is64Bit eh)
+          offset
+        with
+        | .error e => IO.println s!"Note {count} from section {idx} was damaged: {e}"
+        | .ok ne => do
+          let notesize := 0xc + alignTo4 (NoteEntry.note_name ne).size + alignTo4 (NoteEntry.note_desc ne).size
+          if spaceminus - notesize ≥ 0xb
+          then recur (spaceminus - (notesize - 1)) (offset + notesize) (count + 1)
+          else pure ()
+
+def printDynamicErrs
+  [ELFHeader α] (eh : α)
+  (shte: RawSectionHeaderTableEntry)
+  (sec: InterpretedSection)
+  (sidx : Nat)
+  : IO Unit :=
+    for idx in [:SectionHeaderTableEntry.sh_size shte / SectionHeaderTableEntry.sh_entsize shte] do
+      let offset := idx * SectionHeaderTableEntry.sh_entsize shte
+      match mkRawDynamicEntry?
+        sec.section_body
+        (ELFHeader.is64Bit eh)
+        (ELFHeader.isBigendian eh)
+        offset
+      with
+      | .error e => IO.println s!"Dynamic Entry {idx} of section {sidx} was damaged: {e}"
+      | .ok _ => pure ()
+
+def printRelocationAErrs
+  [ELFHeader α] (eh : α)
+  (shte : RawSectionHeaderTableEntry)
+  (sec : InterpretedSection)
+  (sidx : Nat) :=
+  for idx in [:SectionHeaderTableEntry.sh_size shte / SectionHeaderTableEntry.sh_entsize shte] do
+    let offset := idx * SectionHeaderTableEntry.sh_entsize shte
+    match mkRawRelocationA?
+      (sec.section_body)
+      (ELFHeader.is64Bit eh)
+      (ELFHeader.isBigendian eh)
+      offset
+    with
+    | .error e => IO.println s!"Relocation {idx} of section {sidx} was damaged: {e}"
+    | .ok _ => pure ()
+
+def printSymbolErrs
+  [ELFHeader α] (eh : α)
+  (shte: RawSectionHeaderTableEntry)
+  (sec: InterpretedSection)
+  (sidx : Nat) :=
+  for idx in [:SectionHeaderTableEntry.sh_size shte / SectionHeaderTableEntry.sh_entsize shte] do
+    let offset := idx * SectionHeaderTableEntry.sh_entsize shte
+    match mkRawSymbolTableEntry?
+      sec.section_body
+      (ELFHeader.is64Bit eh)
+      (ELFHeader.isBigendian eh)
+      offset
+    with
+    | .error e => IO.println s!"Symbol {idx} of section {sidx} was damaged: {e}"
+    | .ok _ => pure ()
+
+
 def runValidateCmd (p : Cli.Parsed) : IO UInt32 := do
 
   let targetBinary := (p.positionalArg! "targetBinary").as! System.FilePath
   let bytes ← IO.FS.readBinFile targetBinary
 
+  -- retrieve ELF header
   match mkRawELFHeader? bytes with
   | .error warn => do
     IO.println "The ELF Header can't be recovered, so nothing else is readable:"
     IO.println warn *> return 1
   | .ok elfheader => do
 
+  -- retrieve other headers
   let phtEntries := mkRawProgramHeaderErrors elfheader bytes
   let shtEntries := mkRawSectionHeaderErrors elfheader bytes
+
+  -- get section names
+  let maybeNames := getSectionNamesOrErr elfheader shtEntries bytes
+  let tryName (n: Nat) := match maybeNames with | .ok s => .some (s.stringAt n) | .error _ => .none
+
+  match maybeNames with
+  | .ok _ => pure ()
+  | .error e => IO.println s!"The string table listing the section names is unavailable: {e}"
 
   let mut idx := 0
 
@@ -45,15 +156,36 @@ def runValidateCmd (p : Cli.Parsed) : IO UInt32 := do
     idx := idx + 1
 
     match ent with
-      | .error s => IO.println s!"Program Header Table Entry {idx}: {s}"
-      | .ok _ => pure ()
+    | .error s => IO.println s!"Program Header Table Entry {idx}: {s}"
+    | .ok phte =>
+
+    match ProgramHeaderTableEntry.toSegment? phte bytes with
+    | .error s => IO.println s!"Segment Associated with Program Header Table Entry {idx}: {s}"
+    | .ok _ => pure ()
 
   idx := 0
 
   for ent in shtEntries do
     idx := idx + 1
     match ent with
-      | .error s => IO.println s!"Section Header Table Entry {idx}: {s}"
-      | .ok _ => pure ()
+    | .error s => IO.println s!"Section Header Table Entry {idx}: {s}"
+    | .ok shte =>
+
+    match SectionHeaderTableEntry.toSection? shte bytes (tryName idx)  with
+    | .error s => IO.println s!"Section Associated with Section Header Table Entry {idx}: {s}"
+    | .ok sec =>
+
+    if SectionHeaderTableEntry.sh_type shte == ELFSectionHeaderTableEntry.Type.SHT_NOTE
+    then printNotesErrs elfheader shte sec idx
+
+    if SectionHeaderTableEntry.sh_type shte == ELFSectionHeaderTableEntry.Type.SHT_DYNAMIC
+    then printDynamicErrs elfheader shte sec idx
+
+    if SectionHeaderTableEntry.sh_type shte == ELFSectionHeaderTableEntry.Type.SHT_SYMTAB
+       ∨ SectionHeaderTableEntry.sh_type shte == ELFSectionHeaderTableEntry.Type.SHT_DYNSYM
+    then printDynamicErrs elfheader shte sec idx
+
+    if SectionHeaderTableEntry.sh_type shte == ELFSectionHeaderTableEntry.Type.SHT_RELA
+    then printRelocationAErrs elfheader shte sec idx
 
   return 0
--- ELFSage/Types/Section.lean
+++ ELFSage/Types/Section.lean
@@ -4,25 +4,25 @@ import ELFSage.Types.StringTable
 
 structure InterpretedSection where
   /-- Name of the section -/
-  section_name    : Nat 
+  section_name    : Nat
   /-- Type of the section -/
-  section_type    : Nat 
+  section_type    : Nat
   /-- Flags associated with the section -/
-  section_flags   : Nat 
+  section_flags   : Nat
   /-- Base address of the section in memory -/
-  section_addr    : Nat 
+  section_addr    : Nat
   /-- Offset from beginning of file -/
-  section_offset  : Nat 
+  section_offset  : Nat
   /-- Section size in bytes -/
-  section_size    : Nat 
+  section_size    : Nat
   /-- Section header table index link -/
-  section_link    : Nat 
+  section_link    : Nat
   /-- Extra information, depends on section type -/
-  section_info    : Nat 
+  section_info    : Nat
   /-- Alignment constraints for section -/
-  section_align   : Nat 
+  section_align   : Nat
   /-- Size of each entry in table, if section is one -/
-  section_entsize : Nat 
+  section_entsize : Nat
   /-- Body of section -/
   section_body    : ByteArray
   /-- Name of the section, as a string, if the section has one -/
@@ -39,7 +39,7 @@ def SectionHeaderTableEntry.toSection?
   if bytes.size < (sh_offset shte) + (sh_size shte) ∧ hasBits
   then .error $
     s! "{match name with | some n => n | _ => "A section"} specified in the " ++
-    s! "section header table at offset {sh_offset shte}, with size {sh_size shte}" ++
+    s! "section header table at offset 0x{Hex.toHex $ sh_offset shte}, with size 0x{Hex.toHex $ sh_size shte} " ++
     s! "runs off the end of the binary."
   else .ok {
     section_name    := sh_name shte
@@ -58,28 +58,28 @@ def SectionHeaderTableEntry.toSection?
     section_name_as_string := name
   }
 
-def getSectionNames 
-  (shstrndx : Nat) 
-  [SectionHeaderTableEntry α] (sht : List α) 
+def getSectionNames
+  (shstrndx : Nat)
+  [SectionHeaderTableEntry α] (sht : List α)
   (bytes : ByteArray)
   : Except String ELFStringTable := if h : shstrndx ≥ sht.length
     then .error "the shstrndx from the elf header requests a non-existent section"
-    else 
+    else
       let shstr_start := SectionHeaderTableEntry.sh_offset sht[shstrndx]
       let shstr_end := shstr_start + SectionHeaderTableEntry.sh_size sht[shstrndx]
-      if shstr_end > bytes.size 
+      if shstr_end > bytes.size
       then .error "the section header string table runs off the end of the binary"
       else .ok ⟨bytes.extract shstr_start shstr_end⟩
 
 def getInterpretedSections
-  [SectionHeaderTableEntry α] (sht : List α) 
+  [SectionHeaderTableEntry α] (sht : List α)
   [ELFHeader β] (eh : β)
   (bytes : ByteArray)
   : Except String (List (α × InterpretedSection)) := do
   let shstrndx := ELFHeader.e_shstrndx eh
   let section_names ← getSectionNames shstrndx sht bytes
-  sht.mapM $ λshte ↦ 
-    if SectionHeaderTableEntry.sh_name shte == 0 
+  sht.mapM $ λshte ↦
+    if SectionHeaderTableEntry.sh_name shte == 0
     then do
       let sec ← SectionHeaderTableEntry.toSection? shte bytes .none
       return (shte, sec)
